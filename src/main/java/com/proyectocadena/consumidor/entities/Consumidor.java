ce4d7fed04235a271db57da837e9b11d02d4a250
RodrigoDeNA <rodrigovich619@gmail.com>
2025-11-26 19:20:18 +0100

    Primer commit del proyecto * consumidor * para Java con SpringBoot

---
+++ src/main/java/com/proyectocadena/consumidor/entities/Consumidor.java
@@ -0,0 +1,18 @@
+package com.proyectocadena.consumidor.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Consumidor {
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Id
+	private long id;
+	private String name;
+	private String phone;
+
+}
